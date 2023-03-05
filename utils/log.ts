b527a923c051ec9ade1ee361bf8f126f9be9371a
Duy Pham Le <32657584+phamleduy04@users.noreply.github.com>
2023-03-04 23:05:51 -0600

    update deps

--- utils/log.ts
+++ utils/log.ts
@@ -4,23 +4,23 @@ import dayjsUTC from 'dayjs/plugin/utc';
 
 dayjs.extend(dayjsTZ);
 dayjs.extend(dayjsUTC);
-import chalk from 'chalk';
+import { yellow, green, red } from 'colorette';
 
 const timeNow = () =>
     dayjs()
         .tz(process.env.TZ || 'America/Chicago')
         .format('MM/DD/YYYY hh:mm:ss');
 
-const msg = (func, message: string) => func(chalk.yellow(`[${timeNow()}]`) + ' ' + chalk.green(message));
+const msg = (func, message: string) => func(yellow(`[${timeNow()}]`) + ' ' + green(message));
 
 const error = (message = 'Unknown error', err: Error) => {
-    console.error(chalk.yellow(timeNow()) + ' Error: ' + chalk.red(message));
+    console.error(yellow(timeNow()) + ' Error: ' + red(message));
     console.error(err);
 };
 
 const info = (message: string) => {
     msg(console.info, message);
 };
-const warn = (message: string) => msg(console.warn, `${chalk.yellow('WARNING ->')} -> ${message}`);
+const warn = (message: string) => msg(console.warn, `${yellow('WARNING ->')} -> ${message}`);
 
 export { error, info, warn };
